5e5dc08efd46b576c1247519978ae17fc1d98e2b
Dzmitry Patseyuk <dima_pateuk@mail.ru>
2017-09-22 22:14:48 +0300

    configured dev proucess

--- src/view/src/ui/App.jsx
+++ src/view/src/ui/App.jsx
@@ -4,10 +4,10 @@ const React = require('react');
 const Component = require('./Component.jsx');
 
 module.exports = class App extends React.Component {
-    render() {
-        return <div>
-          <h1>Hey</h1>
-          <Component text='Hello' />
-        </div>
-    }
+		render() {
+				return <div>
+					<h1>Hey</h1>
+					<Component text='Hello' />
+				</div>
+		}
 };
--- src/view/src/ui/Component.jsx
+++ src/view/src/ui/Component.jsx
@@ -3,10 +3,10 @@
 const React = require('react');
 
 module.exports = class Component extends React.Component {
-    render() {
-        return <div>
-            <h3>Sub component</h3>
-            {this.props.text || 'no text'}
-        </div>
-    }
+		render() {
+				return <div>
+						<h3>Sub component</h3>
+						{this.props.text || 'no text'}
+				</div>
+		}
 };
